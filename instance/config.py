eb8bd03e96620e031076f52aff8ebf58eef07e48
karolkisinski <karol.kisinski@wp.pl>
2022-08-31 18:53:40 +0200

    created project structure

---
+++ instance/config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/short_test'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
