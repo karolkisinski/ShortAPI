05da1c0f7c9fc73baf890101b11cd7d38bb4c7d8
karolkisinsi <karol.kisinski@wp.pl>
2022-09-20 12:22:32 +0200

    now generating short url

--- app/models.py
+++ app/models.py
@@ -1,19 +1,25 @@
+import secrets
 from app import db
 
 class ShortURL(db.Model):
 
     __tablename__ = 'short_api'
-
+    chars = "ABCDEFGHIJKLMNOQPRSTWUZXV"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     url = db.Column(db.String(255))
     short_url = db.Column(db.String(50))
+    def shortest(self):
+        chars = "ABCDEFGHIJKLMNOQPRSTWUZXV"
+        s_url = "".join(secrets.choice(chars) for _ in range(8))
+        
 
-    def __init__(self, title, url, short_url):
+
+    def __init__(self, title, url):
         self.title = title
         self.url = url
-        self.short_url = short_url
+        self.short_url = "".join(secrets.choice(self.chars) for _ in range(8))
 
     def save(self):
         db.session.add(self)
